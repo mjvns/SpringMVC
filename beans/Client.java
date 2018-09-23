651c1da89232bb7830161143bcd4ba1d8913a71e
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2018-09-23 14:55:08 +0530

    Add files via upload

---
+++ beans/Client.java
@@ -0,0 +1,32 @@
+package beans;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Client {
+	public static void main(String args[]) {
+		//find xml
+		Resource r = new ClassPathResource("resources/Spring.xml");
+		//initialise beanFactory
+		BeanFactory factory = new XmlBeanFactory(r);
+		//instantiate POJO class i.e.Test
+		Test t = (Test)factory.getBean("t");
+		Test t1 = (Test)factory.getBean("t");
+		Test t2 = (Test)factory.getBean("t");
+		
+		if(t == t2) {
+			System.out.println("True");
+		}else {
+			System.out.println("False");
+		}
+		
+		if(t1 == t2) {
+			System.out.println("True");
+		}else {
+			System.out.println("False");
+		}
+		t.hello();
+	}
+}
---
+++ beans/Test.java
@@ -0,0 +1,10 @@
+package beans;
+
+public class Test {
+	public Test() {
+		System.out.println("Test Constructor...");
+	}
+	public void hello() {
+		System.out.println("Hello Spring!!");
+	}
+}
